430203f1dbed8bbef32163c134838fc88bf77f15
Matthew Courtney <cnymhw86@gmail.com>
2022-11-17 14:23:53 -0500

    added first question after start

--- Main/assets/js/logic.js
+++ Main/assets/js/logic.js
@@ -8,14 +8,11 @@
 //THEN I can save my initials and my score
 
 
-
-
-
 var questionsEl = document.getElementById('questions');
 var choicesEl = document.getElementById('choices');
 var startQuizButton = document.getElementById("start");
 var startScreenEl = document.getElementById('start-screen');
-
+var currentQuestionIndex = 0;
 var quizTimer = document.getElementById("time");
 var secondsLeft = 60;
 
@@ -35,7 +32,7 @@ var sfxIncorrect = new Audio('assets/sfx/incorrect.wav');
 // Start Quiz function starts the TimeRanges, hides the start button, and displays the first quiz question.
 function startQuiz(){
   startScreenEl.setAttribute('class', 'hide');
- 
+  generateQuizQuestion();
 
   // un-hide questions section
   questionsEl.removeAttribute('class');
@@ -44,12 +41,36 @@ function startQuiz(){
   
 }
 
+function generateQuizQuestion(){
+  //  getting current question from an array
+   var currentQuestion = questions[currentQuestionIndex];
 
+  //  update title with current question
+  var titleEl = document.getElementById('question-title');
+  titleEl.textContent = currentQuestion.title;
 
+  // clears last question
+   choicesEl.innerHTML = '';
 
+   for (var i = 0; i < currentQuestion.choices.length; i++) {
+    // create new button for each choice referencing the index
+    var choice = currentQuestion.choices[i];
+    var choiceNode = document.createElement('button');
+    choiceNode.setAttribute('class', 'choice');
+    choiceNode.setAttribute('value', choice);
+
+    choiceNode.textContent = i + 1 + '. ' + choice;
+
+    // display choices on the page
+    choicesEl.appendChild(choiceNode);
+  }
+}
+
+// creating button click event
 function questionClick(event) {
   var buttonEl = event.target;
 }
+
   //Timer. 
 function setTime() {
 var timerInterval = setInterval(function() {
@@ -61,6 +82,7 @@ var timerInterval = setInterval(function() {
 
     }, 1000);
 }
+
 
 
 
